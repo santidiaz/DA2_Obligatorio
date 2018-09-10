b4011986dc5e3656b6ca9955d4ce097a18f92f99
Santiago Díaz <santiagodiaz.uy@hotmail.com>
2018-09-10 20:39:05 -0300

    Cambio de nombre de paquetes.

---
+++ Proyecto/SportsManager/MSTests/UnitTests/UserTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UserTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
