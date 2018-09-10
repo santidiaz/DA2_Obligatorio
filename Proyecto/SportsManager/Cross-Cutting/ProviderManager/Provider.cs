79a9dcdf71583e1487200dd831b0ea0794c8368e
Santiago Díaz <santiagodiaz.uy@hotmail.com>
2018-09-10 20:43:43 -0300

    Se agrega ProviderManager.

---
+++ Proyecto/SportsManager/Cross-Cutting/ProviderManager/Provider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ProviderManager
+{
+    public class Provider
+    {
+        //private IUserLogic UserLogic;
+
+        #region Singleton
+        // Variable estática para la instancia, se necesita utilizar una función lambda ya que el constructor es privado.
+        private static readonly Lazy<Provider> currentInstance = new Lazy<Provider>(() => new Provider());
+        private Provider()
+        {
+            //this.UserLogic = new UserLogic(new UserPersistance());
+        }
+        public static Provider GetInstance
+        {
+            get
+            {
+                return currentInstance.Value;
+            }
+        }
+        #endregion
+    }
+}
