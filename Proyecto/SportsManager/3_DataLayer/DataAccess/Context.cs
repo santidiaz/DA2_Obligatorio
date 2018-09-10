d94c2079152fd64c06af47f457697bba1a313af4
Santiago Díaz <santiagodiaz.uy@hotmail.com>
2018-09-10 19:52:36 -0300

    Se agregó proyecto de dataAccess y UnitTesting

---
+++ Proyecto/SportsManager/3_DataLayer/DataAccess/Context.cs
@@ -0,0 +1,22 @@
+﻿using BusinessEntities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess
+{
+    internal class Context : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<User>().HasKey(u => u.UserOID);
+            
+            //modelBuilder.Entity<Teacher>().ToTable("Teachers");
+            //modelBuilder.Entity<Student>().ToTable("Students");
+        }
+    }
+}
---
+++ Proyecto/SportsManager/UnitTesting/Tests/UserTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTesting.Tests
+{
+    [TestClass]
+    public class UserTest
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+
+        }
+    }
+}
