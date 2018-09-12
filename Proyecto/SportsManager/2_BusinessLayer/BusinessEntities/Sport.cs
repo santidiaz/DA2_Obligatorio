4da146bdd65be34ec06cdaf62aa31e8398c3ac38
santilo <santilo17@hotmail.com>
2018-09-12 18:56:35 -0300

    TDD : Se crea entidad Sport y caso de prueba para la creación.

---
+++ Proyecto/SportsManager/2_BusinessLayer/BusinessEntities/Sport.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessEntities
+{
+    public class Sport
+    {
+        public int SportOID { get; set; } // [Object Id] This id is team by EntityFramework.
+        public string Name { get; set; }
+        public List<Team> TeamsList { get; set; } //ver como guardar foto.
+    }
+}
---
+++ Proyecto/SportsManager/MSTests/UnitTests/SportTest.cs
@@ -0,0 +1,26 @@
+﻿using BusinessEntities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class SportTest
+    {
+        [TestMethod]
+        public void CreateSport()
+        {
+            var expectedName = "Futbol";
+            var expectedTeamsList = new List<Team>() { new Team() { Name = "a", Photo = "f" } };
+
+            Sport sport = new Sport();
+            sport.Name = expectedName;
+            sport.TeamsList = expectedTeamsList;
+
+            Assert.AreEqual(sport.Name, expectedName);
+            Assert.AreEqual(sport.TeamsList, expectedTeamsList);
+        }
+    }
+}
