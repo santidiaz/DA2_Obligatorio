965abee05f6a65162df684144d002dcea810c1d5
santilo <santilo17@hotmail.com>
2018-09-12 18:36:15 -0300

    TDD : Se crea entidad Team y caso de prueba para la creación.

---
+++ Proyecto/SportsManager/2_BusinessLayer/BusinessEntities/Team.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessEntities
+{
+    public class Team
+    {
+        public int UserOID { get; set; } // [Object Id] This id is team by EntityFramework.
+        public string Name { get; set; }
+        public string Photo { get; set; } //ver como guardar foto.
+    }
+}
---
+++ Proyecto/SportsManager/MSTests/UnitTests/TeamTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BusinessEntities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class TeamTest
+    {
+        [TestMethod]
+        public void CreateTeam()
+        {
+            var expectedName = "Nacional";
+            var expectedPhoto = "Foto1";
+
+            Team team = new Team();
+            team.Name = expectedName;
+            team.Photo = expectedPhoto;
+
+            Assert.AreEqual(team.Name, expectedName);
+            Assert.AreEqual(team.Photo, expectedPhoto);
+        }
+    }
+}
