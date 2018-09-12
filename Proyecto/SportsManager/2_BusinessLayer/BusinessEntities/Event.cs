09c5979950d276e4927451c9cbb47ce98ac7f8e6
santilo <santilo17@hotmail.com>
2018-09-12 19:20:00 -0300

    TDD : Se crea entidad Event y caso de prueba para la creación.

---
+++ Proyecto/SportsManager/2_BusinessLayer/BusinessEntities/Event.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessEntities
+{
+    public class Event
+    {
+        public int EventOID { get; set; } // [Object Id] This id is Event by EntityFramework.
+        public DateTime Date { get; set; }
+        public Sport Sport { get; set; }
+        public Team Team1 { get; set; }
+        public Team Team2 { get; set; }
+        public string Comments { get; set; } //esto va a ser una lista de Comment
+    }
+}
---
+++ Proyecto/SportsManager/MSTests/UnitTests/EventTest.cs
@@ -0,0 +1,35 @@
+﻿using BusinessEntities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class EventTest 
+    {
+        [TestMethod]
+        public void CreateEvent()
+        {
+            var expectedDate = DateTime.Now;
+            var expectedSport = new Sport();
+            var exceptedTeam1 = new Team();
+            var exceptedTeam2 = new Team();
+            var exceptedComments = "comments";
+
+            Event _event = new Event();
+            _event.Date = expectedDate;
+            _event.Sport = expectedSport;
+            _event.Team1 = exceptedTeam1;
+            _event.Team2 = exceptedTeam2;
+            _event.Comments = exceptedComments;
+
+            Assert.AreEqual(_event.Date, expectedDate);
+            Assert.AreEqual(_event.Sport, expectedSport);
+            Assert.AreEqual(_event.Team1, exceptedTeam1);
+            Assert.AreEqual(_event.Team2, exceptedTeam2);
+            Assert.AreEqual(_event.Comments, exceptedComments);
+        }
+    }
+}
