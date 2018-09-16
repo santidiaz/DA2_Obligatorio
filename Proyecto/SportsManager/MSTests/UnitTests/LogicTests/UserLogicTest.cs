c9824179e88148c0875c3ba6345db5c80a8da70e
Santiago Díaz <santiagodiaz.uy@hotmail.com>
2018-09-16 16:41:13 -0300

    Refactor de DoesUserExists test para validar ambos casos de retorno con moq.

--- Proyecto/SportsManager/MSTests/UnitTests/LogicTests/UserLogicTest.cs
+++ Proyecto/SportsManager/MSTests/UnitTests/LogicTests/UserLogicTest.cs
@@ -17,15 +17,35 @@ namespace UnitTests.LogicTests
             // Creo el objeto mock, en este caso una implementacion mockeada de IUserPersistance.
             var mock = new Mock<IUserPersistance>();
             var mockUserName = "santidiaz";
-            mock.Setup(p => p.DoesUserExists(mockUserName)).Returns(false);
+            mock.Setup(p => p.DoesUserExists(mockUserName)).Returns(true);
 
             // Instancio UserLogic con el mock como parametro.
             UserLogic userLogic = new UserLogic(mock.Object);
-            string userNameToValidate = "santidiaz";
-            bool expectedResult = false;
-            bool result = userLogic.DoesUserExists(userNameToValidate);
 
-            Assert.AreEqual(expectedResult, result);
+            string userNameThatExists = "santidiaz";
+            string userNameThatDoNotExists = "abcdef";
+            bool resultExists = userLogic.DoesUserExists(userNameThatExists);
+            bool resultNotExists = userLogic.DoesUserExists(userNameThatDoNotExists);
+
+            Assert.AreEqual(true, resultExists);
+            Assert.AreEqual(false, resultNotExists);
         }
+
+        //[TestMethod]
+        //public void UserAlreadyExists()
+        //{
+        //    // Creo el objeto mock, en este caso una implementacion mockeada de IUserPersistance.
+        //    var mock = new Mock<IUserPersistance>();
+        //    var mockUserName = "santidiaz";
+        //    mock.Setup(p => p.DoesUserExists(mockUserName)).Returns(true);
+
+        //    // Instancio UserLogic con el mock como parametro.
+        //    UserLogic userLogic = new UserLogic(mock.Object);
+        //    string userNameToValidate = "santidiaz";
+        //    bool expectedResult = true;
+        //    bool result = userLogic.DoesUserExists(userNameToValidate);
+
+        //    Assert.AreEqual(expectedResult, result);
+        //}
     }
 }
