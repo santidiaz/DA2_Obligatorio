71e297debefa2367137c1ff2ea5fb1f21603c831
Santiago Díaz <santiagodiaz.uy@hotmail.com>
2018-09-23 18:32:44 -0300

    TDD: ModifyValidTeams test ok.

--- Proyecto/SportsManager/MSTests/UnitTests/EntitiesTests/EventTest.cs
+++ Proyecto/SportsManager/MSTests/UnitTests/EntitiesTests/EventTest.cs
@@ -47,7 +47,31 @@ namespace UnitTests
             Assert.IsTrue(eventTeams.Contains(team2));
             Assert.IsTrue(newEvent.Sport.Equals(expectedSport));
         }
+        
+        [TestMethod]
+        public void ModifyValidTeams()
+        {
+            DateTime expectedDate = DateTime.Now.AddDays(5);
+            Team team1 = new Team { Name = "Nacional" };
+            Team team2 = new Team { Name = "FC Barcelona" };
+            Team newTema1 = new Team { Name = "Juventus" };
+            Team newTema2 = new Team { Name = "PSG" };
+            List<Team> expectedTeamList = new List<Team> { team1, team2, newTema1, newTema2 };
+            Sport expectedSport = new Sport
+            {
+                Name = "Football",
+                TeamsList = expectedTeamList
+            };
 
+            Event newEvent = new Event(expectedDate, expectedSport, team1, team2);// Original event
+            newEvent.ModifyTeams(newTema1, newTema2);// Method to be tested
+
+            Team[] eventTeams = newEvent.GetTeams();
+            Assert.AreEqual(expectedDate.ToString("dd/MM/yyyy HH:mm:ss"), newEvent.InitialDate.ToString("dd/MM/yyyy HH:mm:ss"));
+            Assert.IsTrue(eventTeams.Contains(newTema1));
+            Assert.IsTrue(eventTeams.Contains(newTema2));
+            Assert.IsTrue(newEvent.Sport.Equals(expectedSport));
+        }
         #region Private Helpers
         #endregion
     }
