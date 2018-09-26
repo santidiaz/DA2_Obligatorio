61c6253e0685f120d554f81b7399adbc931d3240
santilo <santilo17@hotmail.com>
2018-09-26 19:18:53 -0300

    Se agregan metodos a la persistencia de Sport

--- Proyecto/SportsManager/4_DataLayer/DataAccess/Implementations/SportPersistance.cs
+++ Proyecto/SportsManager/4_DataLayer/DataAccess/Implementations/SportPersistance.cs
@@ -1,8 +1,10 @@
 ﻿using BusinessEntities;
 using DataContracts;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace DataAccess.Implementations
 {
@@ -10,32 +12,78 @@ namespace DataAccess.Implementations
     {
         public void AddSport(Sport newSport)
         {
-            throw new NotImplementedException();
+            using (Context context = new Context())
+            {
+                context.Sports.Add(newSport);
+                context.SaveChanges();
+            }
         }
 
         public void DeleteSportByName(string name)
         {
-            throw new NotImplementedException();
+            using (Context context = new Context())
+            {
+                Sport sportToDelete = new Sport() { Name = name };
+                context.Sports.Attach(sportToDelete);
+                context.Sports.Remove(sportToDelete);
+                context.SaveChanges();
+            }
         }
 
         public Sport GetSportByName(string name)
         {
-            throw new NotImplementedException();
+            Sport sportFound;
+            using (Context context = new Context())
+            {
+                var queryResult = (from sport in (context.Sports).Include("Sports")
+                                   where sport.Name.Equals(name)
+                                   select sport).FirstOrDefault();
+
+                sportFound = queryResult;
+            }
+            return sportFound;
         }
 
         public List<Sport> GetSports()
         {
-            throw new NotImplementedException();
+            var sports = new List<Sport>();
+            using (Context context = new Context())
+            {
+                var query = from sport in context.Sports.Include("Sports")
+                            select sport;
+
+                if (query != null)
+                {
+                    foreach (var sport in query)
+                        sports.Add(sport);
+                }
+            }
+            return sports;
         }
 
-        public bool IsSportInSystem(Sport Sport)
+        public bool IsSportInSystem(Sport sport)
         {
-            throw new NotImplementedException();
+            bool result = false;
+            using (Context context = new Context())
+            {
+                var sportOnDB = context.Sports.OfType<Sport>().Include("Sports").Where(a => a.SportOID.Equals(sport.SportOID)).FirstOrDefault();
+
+                result = sportOnDB != null ? true : false;
+            }
+            return result;
         }
 
-        public void ModifySportByName(string name, Sport newSport)
+        public void ModifySportByName(string name, Sport sportToModify)
         {
-            throw new NotImplementedException();
+            using (Context context = new Context())
+            {
+                var sportOnDB = context.Sports.OfType<Sport>().Include("Sports").Where(a => a.SportOID.Equals(sportToModify.SportOID)).FirstOrDefault();
+
+                sportOnDB.Name = sportToModify.Name;
+                sportOnDB.TeamsList = sportToModify.TeamsList;
+
+                context.SaveChanges();
+            }
         }
     }
 }
