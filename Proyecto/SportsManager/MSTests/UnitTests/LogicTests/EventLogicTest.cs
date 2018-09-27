c3c78eda3b7b13f05fa59c7f0d1256d2fa2da056
Santiago Díaz <santiagodiaz.uy@hotmail.com>
2018-09-27 19:50:43 -0300

    TDD: TryAddEventThatDoNotExists test ok

--- Proyecto/SportsManager/MSTests/UnitTests/LogicTests/EventLogicTest.cs
+++ Proyecto/SportsManager/MSTests/UnitTests/LogicTests/EventLogicTest.cs
@@ -55,5 +55,46 @@ namespace UnitTests.LogicTests
                 Assert.Fail(ex.Message);
             }
         }
+
+        [TestMethod]
+        public void TryAddEventThatDoNotExists()
+        {
+            try
+            {
+                #region Initialize Mock
+                var mock = new Mock<IEventPersistance>();
+
+                Team team1 = new Team { Name = "Nacional" };
+                Team team2 = new Team { Name = "Defensor" };
+                Team team3 = new Team { Name = "FC Barcelona" };
+                Team team4 = new Team { Name = "Sevilla" };
+                List<Team> teams = new List<Team> { team1, team2, team3, team4 };
+                Sport sport = new Sport("Football", teams);
+                Event event1 = new Event(DateTime.Now, sport, team1, team3);
+                Event event2 = new Event(DateTime.Now.AddHours(3), sport, team2, team1);
+
+                // Todays event that will be returned.
+                List<Event> todaysMockedEvents = new List<Event> { event1, event2 };
+
+                mock.Setup(up => up.GetTodayEvents()).Returns(todaysMockedEvents);
+                #endregion
+
+                // Instancio UserLogic con el mock como parametro.
+                EventLogic eventLogic = new EventLogic(mock.Object);
+                Event eventToValidate = new Event(DateTime.Now, sport, team1, team2);
+
+                eventLogic.AddEvent(eventToValidate);
+
+                Assert.IsTrue(true);
+            }
+            catch (EntitiesException eEx)
+            {
+                Assert.Fail(eEx.Message);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
     }
 }
