b9c9fd61e177af5c6b9811d73f6882ce2b73f43c
santilo <santilo17@hotmail.com>
2018-09-29 17:09:05 -0300

    Se agrega la entidad Comment

---
+++ Proyecto/SportsManager/3_BusinessLayer/BusinessEntities/Comment.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessEntities
+{
+    public class Comment
+    {
+        private DateTime _datePosted;
+
+        public int CommentOID { get; set; } // [Object Id] This id is used by EntityFramework.
+        public string Description { get; set; } = string.Empty;
+        public string CreatorName { get; set; }
+        public DateTime DatePosted
+        {
+            get { return this._datePosted; }
+        }
+
+        public Comment()
+        {
+            this._datePosted = DateTime.Now;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Comment)
+                return this.DatePosted.Equals(((Comment)obj).DatePosted)
+                    && this.CreatorName.Equals(CreatorName);
+            else
+                return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
