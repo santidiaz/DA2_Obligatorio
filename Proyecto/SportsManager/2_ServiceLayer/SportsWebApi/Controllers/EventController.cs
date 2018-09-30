4b9edbf5a39a649d03f73a2f87763fcdbd6345ef
Santiago Díaz <santiagodiaz.uy@hotmail.com>
2018-09-30 20:48:02 -0300

    Se agrega endpoint AddEvent al controller de Event.

--- Proyecto/SportsManager/2_ServiceLayer/SportsWebApi/Controllers/EventController.cs
+++ Proyecto/SportsManager/2_ServiceLayer/SportsWebApi/Controllers/EventController.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BusinessContracts;
+using BusinessEntities.Exceptions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ProviderManager;
 using ProviderManager.Helpers;
+using SportsWebApi.Models.EventModel;
+using SportsWebApi.Utilities;
 
 namespace SportsWebApi.Controllers
 {
@@ -16,30 +19,34 @@ namespace SportsWebApi.Controllers
     {
         private IEventLogic eventOperations = Provider.GetInstance.GetEventOperations();
 
-        private void algo()
-        {
-            IFixture fixtureGenerationOption = Provider.GetInstance.GetFixtureGenerator(FixtureType.Groups);
-            var generatedEvents = eventOperations.GenerateFixture(fixtureGenerationOption);
-        }
-
-        //[HttpGet("{userName}")]
-        //public IActionResult GetUserByUserName(string userName)
+        //private void algo()
         //{
-        //    try
-        //    {
-        //        if (string.IsNullOrEmpty(userName))
-        //            return NotFound();
-
-        //        User searchedUser = userOperations.GetUserByUserName(userName);
-        //        if (searchedUser == null)
-        //            return NotFound();
-
-        //        return Ok(searchedUser);
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        return this.StatusCode(500, ex.Message);
-        //    }
+        //    IFixture fixtureGenerationOption = Provider.GetInstance.GetFixtureGenerator(FixtureType.Groups);
+        //    var generatedEvents = eventOperations.GenerateFixture(fixtureGenerationOption);
         //}
+
+        [HttpPost()]
+        public IActionResult AddEvent([FromBody] AddEventInput input)
+        {
+            try
+            {
+                if (input == null)
+                    return BadRequest();
+
+                if (input.FirstTeamName.Equals(input.SecondTeamName))
+                    return BadRequest("Teams must be different.");
+
+                eventOperations.AddEvent(input.SportName, input.FirstTeamName, input.SecondTeamName, input.EventDate);
+                return Ok();
+            }
+            catch (EntitiesException eEx)
+            {
+                return this.StatusCode(Utility.GetStatusResponse(eEx), eEx.Message);
+            }
+            catch (Exception ex)
+            {
+                return this.StatusCode(500, ex.Message);
+            }
+        }
     }
 }
---
+++ Proyecto/SportsManager/2_ServiceLayer/SportsWebApi/Models/EventModel/AddEventInput.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsWebApi.Models.EventModel
+{
+    public class AddEventInput
+    {
+        [Required]        
+        public string SportName { get; set; }
+
+        [Required]
+        public string FirstTeamName { get; set; }
+
+        [Required]
+        public string SecondTeamName { get; set; }
+
+        [Required]
+        public DateTime EventDate { get; set; }
+    }
+}
