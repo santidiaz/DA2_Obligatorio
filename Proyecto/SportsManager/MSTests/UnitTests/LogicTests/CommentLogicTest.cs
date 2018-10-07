3c6b0ce7d53089d417a1973e63afdf67f9352720
santilo <santilo17@hotmail.com>
2018-10-07 13:43:14 -0300

    REFACTOR : Se ajusta logica de comentarios y se completa la cobertura

--- Proyecto/SportsManager/MSTests/UnitTests/LogicTests/CommentLogicTest.cs
+++ Proyecto/SportsManager/MSTests/UnitTests/LogicTests/CommentLogicTest.cs
@@ -1,4 +1,5 @@
 ﻿using BusinessEntities;
+using BusinessEntities.Exceptions;
 using BusinessLogic;
 using CommonUtilities;
 using DataContracts;
@@ -56,6 +57,32 @@ namespace UnitTests.LogicTests
                 Assert.IsTrue(ex.Message.Equals(Constants.CommentError.ERROR_CREATOR_NAME_NOT_EXISTS));
             }
 
+        }
+
+        [TestMethod]
+        public void AddCommentThatEventNotExists()
+        {
+            try
+            {
+                // Creo el objeto mock, en este caso una implementacion mockeada de ISportPersistance.
+                var mock = new Mock<ICommentPersistance>();
+                var mockEvent = new Mock<IEventPersistance>();
+                mock.Setup(mr => mr.UserCreatorExists(It.IsAny<string>())).Returns(true);
+                mock.Setup(mr => mr.AddComment(It.IsAny<Comment>(), 1)).Verifiable();
+                mockEvent.Setup(mr => mr.GetAllEvents()).Returns(new List<Event>() { new Event() { EventOID = 2 } });
+
+
+                // Instancio SportLogic con el mock como parametro.
+                CommentLogic userLogic = new CommentLogic(mock.Object, mockEvent.Object);
+                Comment commentToAdd = new Comment() { Description = "Comment1", CreatorName = "goku" };
+                userLogic.AddComment(commentToAdd, 1);
+                
+                Assert.IsTrue(true);
+            }
+            catch (EntitiesException eEx)
+            {
+                Assert.IsTrue(eEx.Message.Equals(Constants.EventError.NOT_FOUND));
+            }
         }
     }
 }
