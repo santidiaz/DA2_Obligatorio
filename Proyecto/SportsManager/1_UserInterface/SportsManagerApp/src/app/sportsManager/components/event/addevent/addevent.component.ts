bdc4f9f4c7136837fc5f34404d2b1d8fd5ee334f
santilo <santilo17@hotmail.com>
2018-11-12 14:09:12 -0300

    Se agrega pagina para agregar evento

---
+++ Proyecto/SportsManager/1_UserInterface/SportsManagerApp/src/app/sportsManager/components/event/addevent/addevent.component.ts
@@ -0,0 +1,12 @@
+import { BaseComponent } from "src/app/sportsManager/shared/base.component";
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-addevent',
+    templateUrl: './addevent.component.html',
+    styleUrls: ['./addevent.component.css']
+  })
+  
+  export class AddEventComponent extends BaseComponent {
+
+}
