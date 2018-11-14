72f3a80819a848d1ed3495f8b2d710d3b3444d99
santilo <santilo17@hotmail.com>
2018-11-14 18:25:17 -0300

    Se agrega pagina de dynamic para cargar fixture

--- Proyecto/SportsManager/1_UserInterface/SportsManagerApp/src/app/sportsManager/components/event/addevent/addevent.component.ts
+++ Proyecto/SportsManager/1_UserInterface/SportsManagerApp/src/app/sportsManager/components/event/addevent/addevent.component.ts
@@ -1,26 +1,32 @@
 import { BaseComponent } from "src/app/sportsManager/shared/base.component";
 import { Component } from "@angular/core";
 import { EventRequest } from "src/app/sportsManager/interfaces/event-request";
+import { Event } from '../../../interfaces/event'
 import { SessionService } from "src/app/sportsManager/services/session.service";
 import { EventService } from "src/app/sportsManager/services/event.service";
+import { EventRequestDynamic } from "src/app/sportsManager/interfaces/event-request-dynamic";
+import { FixtureService } from "src/app/sportsManager/services/fixture.services";
 
 @Component({
     selector: 'app-addevent',
     templateUrl: './addevent.component.html',
     styleUrls: ['./addevent.component.css']
-  })
-  
-  export class AddEventComponent extends BaseComponent {
+})
+
+export class AddEventComponent extends BaseComponent {
 
     isManualFormChecked: boolean;
     isDynamicFormChecked: boolean;
     eventsToAdd: Array<EventRequest> = [];
+    auxEventFromDynamic: Array<Event> = [];
+    auxEvent: EventRequest;
 
     constructor(
         private sessionService: SessionService,
-        private eventService: EventService) {
+        private eventService: EventService,
+        private fixtureService: FixtureService) {
         super();;
-      };
+    };
 
     ngOnInit() {
         //this.updateGrid();
@@ -30,21 +36,38 @@ import { EventService } from "src/app/sportsManager/services/event.service";
     }
 
     changeView($event, item: Number) {
-        if(item == 1)
-        {
+        if (item == 1) {
             this.isManualFormChecked = true;
             this.isDynamicFormChecked = false;
         }
-        else 
-        {
+        else {
             this.isManualFormChecked = false;
             this.isDynamicFormChecked = true;
         }
-      }
+    }
 
-      addEventTemp(EventRequest: EventRequest) {
+    addEventTemp(EventRequest: EventRequest) {
 
         this.eventsToAdd.push(EventRequest);
+    }
+
+    addEventTempDynamic(eventsRequest: EventRequestDynamic) {
+
+        // buscar los eventos a crear.
+        this.fixtureService.getGenerateFixtures(eventsRequest).subscribe(response => {
+            //this.auxEventFromDynamic = ;
+            response.forEach(event => {
+                this.auxEvent = { sportName: null, eventDate: null, teamNames: []};
+                
+                this.auxEvent.eventDate = event.initialDate;
+                this.auxEvent.sportName = event.sportName;
+                event.teams.forEach(tem => { this.auxEvent.teamNames.push(tem.name) });
+    
+                this.eventsToAdd.push(this.auxEvent);
+            });
+          });
+
+        
     }
 
     deleteEvent($event, event: EventRequest) {
@@ -55,23 +78,23 @@ import { EventService } from "src/app/sportsManager/services/event.service";
     }
 
     onSubmit() {
-        
+
         this.eventsToAdd.forEach(element => {
             this.eventService.addEvent(element).subscribe(
                 response => this.handleResponse(response),
                 error => this.handleError(error));
         });
-      }
+    }
 
-      private handleResponse(response: any) {
+    private handleResponse(response: any) {
         //this.sessionService.setSession(response);
         //this.errorMessage = null;
         //this.successMessage = 'Operation success';
-      }
-    
-      private handleError(error: any) {
+    }
+
+    private handleError(error: any) {
         //this.successMessage = null;
         //this.errorMessage = error.error;
-      }
+    }
 
 }
